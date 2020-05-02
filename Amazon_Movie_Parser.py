030f3ab11d028bc4549430fb056b2ba5005417fb
Melih Zenciroglu <54543965+asmelzene@users.noreply.github.com>
2020-05-02 12:24:51 +0200

    Add files via upload
    the data source used:
    https://snap.stanford.edu/data/web-Movies.html
    
    the below file was unzipped and since it was too big to open, a parser was written to save a part of it after unzipping the file on the harddisk as movies.txt
    
    movies.txt.gz	Amazon movie data (~8 million reviews)
    
    A view of the data
    product/productId: B00006HAXW
    review/userId: A1RSDE90N6RSZF
    review/profileName: Joseph M. Kotow
    review/helpfulness: 9/9
    review/score: 5.0
    review/time: 1042502400
    review/summary: Pittsburgh - Home of the OLDIES
    review/text: I have all of the doo wop DVD's and this one is as good or better than the
    1st ones. Remember once these performers are gone, we'll never get to see them again.
    Rhino did an excellent job and if you like or love doo wop and Rock n Roll you'll LOVE
    this DVD !!

---
+++ Amazon_Movie_Parser.py
@@ -0,0 +1,160 @@
+import fileinput
+import numpy as np
+import datetime
+
+def AmazonMovies(num_reviews, file_name="movies.txt", outputTo='screen'):
+    '''
+    *1) product/productId: B003AI2VGA
+    *2) review/userId: A141HP4LYPWMSR
+    *3) review/profileName: Brian E. Erland "Rainbow Sphinx"
+    4) review/helpfulness: 7/7
+    *5) review/score: 3.0
+    6) review/time: 1182729600
+    7) review/summary: "There Is So Much Darkness Now ~ Come For The Miracle"
+    8) review/text: Synopsis: On the daily..............
+    9) null
+    '''
+    i = 0
+    single_entry=np.zeros(4, str)
+    single_entry_completed = True
+
+    movie_data = np.array([["product/productId", "review/userId", "review/profileName", "review/score"]])
+    #movie_data = np.array([["product/productId", "review/userId", "review/profileName", \
+     #                       "review/helpfulness", "review/score"]])
+    num_rec = 0
+
+    ## ***** We don't necessarilly need helpfulness, just keeping it in case we use it, otherwise, we can remove it
+    # encoding="latin-1 >> is needed otherwise possible to receive:
+    # UnicodeDecodeError: 'utf8' codec can't decode byte 0x9c
+    # https://stackoverflow.com/questions/12468179/unicodedecodeerror-utf8-codec-cant-decode-byte-0x9c
+    # reading/writing the file with "with"is also important since "with"handles opening/closing the file
+    # after finishing its job or even if there is an exception thrown
+    with open(file_name, encoding="latin-1") as input_file:
+        for line in input_file:
+            i += 1
+            #for each entry, repeats at every 9
+            if (i % 9 == 1):
+                # just to be sure we are not carrying anything from the previous record, we reset the single_entry
+                #single_entry = np.zeros(4, str)
+                single_entry=np.array([["productId", "userId", "profileName", "score"]])
+                #single_entry=np.array([["productId", "userId", "profileName", "helpfulness", "score"]])
+                single_entry[0, 0] = line[19:].rstrip("\n")
+            elif (i % 9 == 2):
+                single_entry[0, 1] = line[15:].rstrip("\n")
+            elif (i % 9 == 3):
+                single_entry[0, 2] = line[20:].rstrip("\n")
+            elif (i % 9 == 5):
+                single_entry[0, 3] = line[14:].rstrip("\n")
+            elif (i % 9 == 0):
+                num_rec += 1
+                #['B003AI2VGA\n' 'A141HP4LYPW' 'Brian E. Er' '3.0\n']
+                movie_data = np.append(movie_data, single_entry, axis = 0)
+
+            if (num_rec == num_reviews):
+                fileinput.close()
+                break
+
+    if outputTo == 'file':
+        # to avoid overwriting an existing file, if you want to overwrite, just basically use output.txt
+        file_name = FileNameUnique() 
+        #with open("output.txt", "w") as output:
+        with open(file_name, "w") as output:
+            for movie in movie_data:
+                output.write(str(movie) + "\n")
+    elif outputTo == 'screen':
+        #if you just want to show it here, uncomment to above part
+        for movie in movie_data:
+            print(str(movie) + "\n")
+    elif outputTo == 'dictionary':
+        movie_dict = {}
+        key = 0
+        for movie in movie_data:
+            movie_dict[key] = [movie[0], movie[1], movie[2], movie[3]]
+            key += 1
+        del movie_dict[0]
+        return movie_dict
+def Time_Stamp():
+    date_time = datetime.datetime.now()
+    
+    D = str(date_time.day)
+    M = str(date_time.month)
+    Y = str(date_time.year)
+
+    h = str(date_time.hour)
+    m = str(date_time.minute)
+    s = str(date_time.second)
+    
+    date_array = [D, M, Y, h, m, s]
+    
+    return date_array
+    
+def FileNameUnique(prefix = "Movies_"):
+    file_name = prefix
+
+    date_array = Time_Stamp()
+    
+    for idx, i in enumerate(date_array):
+        if idx == 2:
+            file_name += i + '_'
+        elif idx == 5:
+            file_name += i + '.txt'
+        else:
+            file_name += i + '.'
+
+    #print(file_name)
+    return file_name
+                
+def Save_Movies(movie_graph, prefix = "Movies_Connected_"):
+    # ('userID', 'movieID', 'score')
+    # ('A141HP4LYPW', 'B003AI2VGA', '3.0')
+    movie_list = list(movie_graph.edges.data('weight', default=1))
+    
+    file_name = prefix + str(len(movie_list)) + "_"
+    date_array = Time_Stamp()
+    
+    for idx, i in enumerate(date_array):
+        if idx == 2:
+            file_name += i + '_'  # YEAR
+        elif idx == 5:
+            file_name += i + '.txt'  # SECOND
+        else:
+            file_name += i + '.'
+ 
+    # since the graph is undirected, once the edges is created as (u, v, weight)
+    # it was observed that some edges are saved in the graph as (v, u, weight) # graph.edges returns some edges in that way
+    # so, for the second use, to make the life easier for us, we save them by checking if they are in the right order
+    with open(file_name, "w") as output:
+            for movie in movie_list:
+                if movie[0].startswith('A'):
+                    userId = movie[0]
+                    movieId = movie[1]
+                elif movie[0].startswith('B'):
+                    userId = movie[1]
+                    movieId = movie[0]
+                str_movie = userId + "," + movieId + "," + movie[2]
+                #output.write(str(movie) + "\n")
+                output.write(str_movie + "\n")
+    
+def Read_Connected_Movies_x(file_name):
+    #movie_list = np.array([["userId", "productId", "score"]])
+    movie_list = np.array([('userID', 'movieID', 'score')])
+    print(movie_list[0])
+    
+    with open(file_name, encoding="latin-1") as input_file:
+        for line in input_file:
+            print(line)
+            movie_list = np.append(movie_list, line, axis = 0)
+               
+    return list(movie_list)
+
+def Read_Connected_Movies(file_name):
+    #movie_list = np.array([["userId", "productId", "score"]])
+    movie_list = []
+    
+    with open(file_name, encoding="latin-1") as input_file:
+        #movie_list = input_file.read().splitlines()
+        #movie_list = input_file.readlines()
+        for line in input_file:
+            movie_list.append(line.rstrip("\n").split(sep=","))
+               
+    return movie_list
---
+++ Graph_Amazon_Movies.py
@@ -0,0 +1,119 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import Amazon_Movie_Parser as prs
+from networkx.algorithms import bipartite
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+#import datetime
+#import time
+
+class Graph_Amazon:
+    def __init__(self):
+        #self.label_size = 10
+        #self.image_size = 32 * 32 * 3
+        #for one_hot_encoding
+        #self.label_encoder = preprocessing.LabelBinarizer()
+        # file path of the images on your laptop
+        #self.filePath = 'Dataset/data_batch_1'
+        #self.unique_labels = []
+        self.file_name = 'movies.txt'
+        
+    def Create_and_Draw_Bipartite(self, file_name = 'movies.txt', n_movies = 200, prs_out = 'dictionary'):
+        #prs_out = 'dictionary'  # options: screen, file.  # file name is determined automatically if file is chosen
+        movie_dict = prs.AmazonMovies(n_movies, file_name, prs_out)
+
+        movie_name = []
+        userId = []
+        edges_U_M = []
+
+        gr_amazon_movies = nx.Graph()
+        gr_amazon_movies.add_nodes_from(userId, bipartite=0)
+        gr_amazon_movies.add_nodes_from(movie_name, bipartite=1)
+        #gr_amazon_movies.add_edges_from(edges_U_M)
+
+        for movie in movie_dict:
+            gr_amazon_movies.add_edge(movie_dict.get(movie)[1], movie_dict.get(movie)[0], weight=movie_dict.get(movie)[3])
+
+        max_connected_gr_amazon_movies = max(nx.connected_component_subgraphs(gr_amazon_movies), key=len)
+        
+        self.bottom_nodes, self.top_nodes = bipartite.sets(max_connected_gr_amazon_movies)
+
+        #self.top = nx.bipartite.sets(max_connected_gr_amazon_movies)[0]
+        pos = nx.bipartite_layout(max_connected_gr_amazon_movies, self.top_nodes)
+        nx.draw(max_connected_gr_amazon_movies, pos, with_labels=1)
+        nx.draw_networkx_edge_labels(max_connected_gr_amazon_movies, pos)
+        #nx.is_connected(G)
+
+        plt.show()
+        
+        return max_connected_gr_amazon_movies
+    
+    def Create_Bipartite(self, file_name = 'movies.txt', n_movies = 200, prs_out = 'dictionary'):
+        #prs_out = 'dictionary'  # options: screen, file.  # file name is determined automatically if file is chosen
+        if prs_out == 'dictionary':
+            movie_dict = prs.AmazonMovies(n_movies, file_name, prs_out)
+
+            #movie_name = []
+            #userId = []
+            
+            gr_amazon_movies = nx.Graph()
+            #gr_amazon_movies.add_nodes_from(userId, bipartite=0)
+            #gr_amazon_movies.add_nodes_from(movie_name, bipartite=1)
+
+            # userId, productId, score
+            for movie in movie_dict:
+                print(movie_dict.get(movie))
+                # usedId__(movie)[1] > movieId__(movie)[0]
+                gr_amazon_movies.add_edge(movie_dict.get(movie)[1], movie_dict.get(movie)[0], weight=movie_dict.get(movie)[3])
+
+            self.gr_amazon_movies = gr_amazon_movies
+                
+            max_connected_gr_amazon_movies = max(nx.connected_component_subgraphs(gr_amazon_movies), key=len)
+
+            self.bottom_nodes, self.top_nodes = bipartite.sets(max_connected_gr_amazon_movies)
+
+            return max_connected_gr_amazon_movies
+        elif prs_out == 'file' or prs_out == 'screen':
+            prs.AmazonMovies(n_movies, file_name, prs_out)
+    
+    def Draw_Bipartite(self, max_connected_gr_amazon_movies):
+        #self.top = nx.bipartite.sets(max_connected_gr_amazon_movies)[0]
+        pos = nx.bipartite_layout(max_connected_gr_amazon_movies, self.top_nodes)
+        nx.draw(max_connected_gr_amazon_movies, pos, with_labels=1)
+        nx.draw_networkx_edge_labels(max_connected_gr_amazon_movies, pos)
+
+        plt.show()
+        
+    def Show_Nodes(self):
+        print('Sum-Top Nodes:{}\nTop nodes (movies): {}'.format(len(self.top_nodes), self.top_nodes))
+        print('Sum-Bottom Nodes:{}\nBottom nodes (users):\n {}'.format(len(self.bottom_nodes), self.bottom_nodes))
+    
+    def Create_Graph_From_List_No_Weight(self, movie_list):
+        userId = []
+        movie_name = []
+            
+        for movie in movie_list:
+            userId.append(movie[0])
+            movie_name.append(movie[1])
+            
+        gr_amazon_movies = nx.Graph() 
+        gr_amazon_movies.add_nodes_from(userId, bipartite=0)
+        gr_amazon_movies.add_nodes_from(movie_name, bipartite=1)
+        
+        return gr_amazon_movies
+        
+    def Get_All_Edges_with_Weight(self, max_connected_gr_amazon_movies):
+        #lst_x=list(max_connected_gr_amazon_movies.edges.data())
+        #max_connected_gr_amazon_movies.edges('AMEJTGF5NQL')
+        #edge_view_grp = max_connected_gr_amazon_movies.edges.data('weight', default=1)
+        #edge_view_grp[0]
+        # https://networkx.github.io/documentation/stable/reference/classes/generated/networkx.Graph.edges.html
+        edge_list = list(max_connected_gr_amazon_movies.edges.data('weight', default=1))
+        return edge_list
+    
+    def Is_Connected_Bipartite(self, max_connected_gr_amazon_movies):
+        print('Is connected: {}'.format(nx.is_connected(max_connected_gr_amazon_movies)))
+        print('Is bipartite: {}'.format(nx.is_bipartite(max_connected_gr_amazon_movies)))
